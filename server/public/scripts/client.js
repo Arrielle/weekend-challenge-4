40809e11182933b3bb538354bbb896953198473c
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-04 01:50:57 -0600

    tasks from database have been added to the list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,14 +2,32 @@ console.log('javascript');
 
 $(document).ready(function(){
 
-  var taskIsComplete = false;
+getTaskDataAddToDom();
 
+function getTaskDataAddToDom(){
   $.ajax({
     type: 'GET',
     url: '/getTasks',
     success: function(response){
-      console.log('Here are the tasks that were "got"', response);
+      for (var i = 0; i < response.length; i++) {
+        var todoList = response[i];
+        if (todoList.complete === true){
+          $('#completedTaskTableBody').append('<tr><td>' +
+            '<div id = "completedTaskDescription">' + todoList.task_description + '</div></td>' +
+            '<td><div type="button" name="delete" class="box" >' +
+              '<div class="box-left"><i class="but-icon fa fa-lg fa-times"></i></div>' +
+            '</div></td>' +
+            '<td><div type="button" name="smile" class="box" >' +
+              '<div class="box-left, smileButton"><i class="but-icon fa fa-lg fa-smile-o"></i></div>' +
+            '</div></td></tr>');
+        } else if (todoList.complete === false){
+          $('#taskTableBody').append('<tr><td><div id = "toDo">' +
+          todoList.task_description +
+          '</div></td><td><div type="button" name="delete" class="box" ><div class="box-left"><i class="but-icon fa fa-lg fa-times"></i></div></div></td>' +
+              '<td><div type="button" name="complete" class="box" ><div class="box-left, completeButton"><i class="but-icon fa fa-lg fa-check"></i></div></div></td></tr>');
+        }//ends else if
+      }//ends for loop
     }//ends success ajax
   });//ends GET ajax
-
-});
+}//ends function getTaskDataAddToDom
+});//ends doc.ready
