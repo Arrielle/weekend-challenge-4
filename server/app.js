0640712f085771f4b433638e0c8bce66f0e0810d
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-04 00:12:22 -0600

    added some styling

--- server/app.js
+++ server/app.js
@@ -16,6 +16,29 @@ var config = {
   idleTimeoutMillis: 30000 // 30 seconds to try to connect
 };
 
+app.get('/getTasks', function(req, res){
+  pool.connect(function(err, client, done){
+    if(err) {
+      // There was an error connecting to the database
+      console.log('Error connecting to database: ', err);
+      res.sendStatus(500);
+    } else {
+      // We connected to the database!!!
+      // Now, we're gonna' git stuff!!!!!
+      client.query('SELECT owners.first_name, owners.last_name, pets.pet_name, pets.pet_breed, pets.pet_color, visits.checkin, visits.checkout FROM owners FULL OUTER JOIN pets ON owners.id = pets.owners_id FULL OUTER JOIN visits ON visits.pets_id=pets.id;', function(err, result){
+        done();
+        if(err) {
+          console.log('Error making the database query: ', err);
+          res.sendStatus(500);
+        } else {
+          res.send(result.rows);
+          console.log(result.rows);
+        }
+      });
+    }
+  });
+});//ends app.gets
+
 app.listen(port, function() {
   console.log('We are running on port: ', port);
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,24 @@
 console.log('javascript');
 
 $(document).ready(function(){
-  console.log('jquery is sourced');
+
+  var taskIsComplete = false;
+
+//   $(".box").hover(function() {
+//   $(".box-right").toggleClass('cl-box2');
+//   $(".bar").toggleClass('cl-bar2');
+// });
+//
+// $(".bar").click(function() {
+//   alert("Deleted");
+// });
+
+  $.ajax({
+    type: 'GET',
+    url: '/getTasks',
+    success: function(response){
+      console.log('Here are the tasks that were "got"', response);
+    }//ends success ajax
+  });//ends GET ajax
+
 });
